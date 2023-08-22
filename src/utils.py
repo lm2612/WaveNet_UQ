5f103aeac238cc8450c15b8101d997bce8b296b6
lm2612 <lauraman@stanford.edu>
2023-08-21 17:09:45 -0700

    add nc suffix to end of file name

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+def check_nc(filename):
+    """Check filename returns .nc file, otherwise add .nc"""
+    if filename[-3:]!=".nc":
+        return f"{filename}.nc"
+    else:
+        return filename
+
 def init_xavier(m):
     if type(m) == nn.Conv2d or type(m) == nn.Linear or type(m) == nn.ConvTranspose2d:
         nn.init.xavier_uniform_(m.weight)
