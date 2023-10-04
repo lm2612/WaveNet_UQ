bfe099bf597993f15d861f9ecb2e1ae99b70e30f
lm2612 <lauraman@stanford.edu>
2023-10-04 13:22:12 -0700

    negative log likelidhood fixed

--- src/custom_loss.py
+++ src/custom_loss.py
@@ -1,10 +1,16 @@
+import numpy as np 
 import torch
 
-
 def neg_log_likelihood(Y, mu, sigma):
-    return -torch.sum( torch.log(sigma**2)/2 + (Y-mu)**2/(2*sigma**2))
+    N = Y.shape[0]
+    log_likelihood = - (1/2) * torch.log(2*np.pi*sigma**2)  - (1/N)* torch.sum((Y-mu)**2/(2*sigma**2) , axis=0)
+    return -torch.mean(log_likelihood)
+
 
 def neg_log_likelihood_loss(Y_pred, Y):
     mu = Y_pred[:, 0, :]
     std = Y_pred[:, 1, :]
     return neg_log_likelihood(Y, mu, std)
+
+
+
