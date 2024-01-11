e34423b5b49fafe2226ace3e4df324dc6380439f
lm2612 <lauraman@stanford.edu>
2024-01-10 16:05:47 -0800

    add PV winds calculated in similar way to QBO winds. Also added arguments to parse for the long run, saved as filenum 1 , 2, 3 etc. rather than one very long file

--- analysis/save_PV_winds.py
+++ analysis/save_PV_winds.py
@@ -22,25 +22,32 @@ base_dir = "/scratch/users/lauraman/WaveNetPyTorch/mima_runs"
 print(f"Opening file for {dir_name}")
 ds = xr.open_dataset(f"{base_dir}/{dir_name}/atmos_daily_{t_start}.nc", decode_times=False)
 
-# Dataset at equator 
-ds_tropics = ds.sel(lat=slice(-5,5))
-lat = ds_tropics["lat"]
-time = ds_tropics["time"]
+# Dataset NH
+ds_NH = ds.sel(lat=slice(50,90))
+ds_SH = ds.sel(lat=slice(-90,-50))
 
-# Take area-weighted mean across all variables
-weights = np.cos(np.deg2rad(lat))
-ds_QBO = ds_tropics.weighted(weights).mean(("lon","lat"))
-print(ds_QBO)
+# Zonal mean across all variables
+ds_NHPV = ds_NH.mean(("lon"))
+ds_SHPV = ds_SH.mean(("lon"))
 
 # Extend time series to all years
-for t in range(start_year+1, end_year+1):
+for t in range(t_start, t_end):
     ds = xr.open_dataset(f"{base_dir}/{dir_name}/atmos_daily_{t}.nc", decode_times=False)
-    ds_tropics = ds.sel(lat=slice(-5,5))
-    ds_QBO = xr.concat( (ds_QBO, ds_tropics.weighted(weights).mean(("lon","lat")) ), dim="time")
+    ds_NH = ds.sel(lat=slice(50,90))
+    ds_NHPV = xr.concat( (ds_NHPV, ds_NH.mean(("lon")) ), dim="time")
+
+    ds_SH = ds.sel(lat=slice(-90,-50))
+    ds_SHPV =  xr.concat( (ds_SHPV, ds_SH.mean(("lon")) ), dim="time")
+
 
 # Save
-print(ds_QBO)
-file_out = f"{base_dir}/{dir_name}/QBO_winds{file_num}.nc"
-ds_QBO.to_netcdf(path=file_out, mode='w')
+print(ds_NH)
+file_out = f"{base_dir}/{dir_name}/PV_NH_winds{file_num}.nc"
+ds_NHPV.to_netcdf(path=file_out, mode='w')
+print(f"Saved to {file_out}")
+
+print(ds_SH)
+file_out = f"{base_dir}/{dir_name}/PV_SH_winds{file_num}.nc"
+ds_SHPV.to_netcdf(path=file_out, mode='w')
 print(f"Saved to {file_out}")
 
