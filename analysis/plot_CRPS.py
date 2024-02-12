892b66f2912fbe21ede8c753dbea3794680a28d7
lm2612 <lauraman@stanford.edu>
2024-02-12 12:45:55 -0800

    more informative comments, added a) b) labels

--- analysis/plot_CRPS.py
+++ analysis/plot_CRPS.py
@@ -68,29 +68,20 @@ for n, seed in enumerate(seeds):
     ds_u = xr.open_dataset(f"{ML_dir}/{filename}", decode_times=False )
     gwfv_preds[n] = ds_u["gwfv_cgwd_pred"][:ntime:subsample_time, :, ::subsample_lat, ::subsample_lon]
 
-## Reshape: lon and time axes merge
+## Reshape: We want data points to be on first axes (lon and time axes merge)
 gwfu_preds = np.moveaxis(gwfu_preds, -1, 1).reshape((n_seeds, (ntime//subsample_time)*(nlon//subsample_lon), npfull, nlat//subsample_lat) )
 gwfv_preds = np.moveaxis(gwfv_preds, -1, 1).reshape((n_seeds, (ntime//subsample_time)*(nlon//subsample_lon), npfull, nlat//subsample_lat) )
 gwfu_ad99 = np.moveaxis(gwfu_ad99, -1, 0).reshape(( (ntime//subsample_time)*(nlon//subsample_lon), npfull, nlat//subsample_lat) )
 gwfv_ad99 = np.moveaxis(gwfv_ad99, -1, 0).reshape(( (ntime//subsample_time)*(nlon//subsample_lon), npfull, nlat//subsample_lat) )
    
 
-## Error metrics: MAE, RMSE, R2, CRPS
-## arrays are of size (N_ensemble, N_samples (time/lat/lon), pfull)
-def MAE(y_true, y_pred, sample_weight=None):
-    if y_pred.ndim > y_true.ndim:
-        y_pred = np.mean(y_pred, axis=0) 
-    y_pred = np.mean(y_pred, axis=0)
-    mean_absolute_error = np.average(np.abs(y_pred - y_true), axis=0, weights=sample_weight)
-    return mean_absolute_error
-
-def RMSE(y_true, y_pred, sample_weight=None):
-    if y_pred.ndim > y_true.ndim:
-        y_pred = np.mean(y_pred, axis=0) 
-    mean_squared_error = np.average((y_pred - y_true)**2, axis=0, weights=sample_weight)
-    return np.sqrt(mean_squared_error)
-
 def crps(y_true, y_pred, sample_weight=None):
+    """Calculate Continuous Ranked Probability Score
+    Data based on size (N, np_full) where N=number of samples (in time, lat, lon) and np_full is height (=40) 
+    Args:
+     * y_true : np.array (N, np_full) ground truth from AD99 for N samples, for profile of height np_full
+     * y_pred : np.array (n_seeds, N, np_full) predicted from n_seeds ensembles, for N samples, for profile of height np_full
+    """
     num_samples = y_pred.shape[0]
     y_pred = np.sort(y_pred, axis=0)
     diff = y_pred[1:] - y_pred[:-1]
@@ -101,82 +92,62 @@ def crps(y_true, y_pred, sample_weight=None):
     per_obs_crps = absolute_error - np.sum(diff * weight, axis=0) / num_samples**2
     return np.average(per_obs_crps, axis=0, weights=sample_weight)
 
-def r2(y_true, y_pred, sample_weight=None):
-    if y_pred.ndim > y_true.ndim:   
-        y_pred = np.mean(y_pred, axis=0) 
-    y_mean = np.mean(y_true, axis=0)
-    ss_tot = np.sum((y_true-y_mean)**2, axis=0)
-    ss_res = np.sum((y_true-y_pred)**2, axis=0)
-    return 1 - (ss_res/ss_tot)
-
-
-def plot_error_profile(err_u, err_v, xmin=0, xmax=1):
-    fig, axs = plt.subplots(1, 2 , figsize=(10, 5), sharey = True)
-    # Zonal
-    ax = axs[0]
-    plt.sca(ax)
-    # Truth
-    plt.semilogy(err_u, pfull,  color="black")
-    plt.xlabel("Zonal GWD (ms$^{-2}$)")
-    plt.title("Zonal GWD")
-    ax.invert_yaxis()
-    plt.ylabel("Pressure (hPa)")
-    plt.axis(xmin=xmin, xmax=xmax, ymin=1e2, ymax=1)
-
-    # Meridional
-    ax = axs[1]
-    plt.sca(ax)
-    plt.semilogy(err_v,  pfull,    color="black")
-    plt.xlabel("Meridional GWD (ms$^{-2}$)")
-    plt.title("Meridional GWD")
-    plt.axis(xmin=xmin, xmax=xmax)
-    return fig
-
-
-# CRPS
+# CRPS plot
 plt.clf()
-## equator
+# Select latitude bands for subseting data and plotting
 labels = ["<85$\degree$S", "55$\degree$-65$\degree$S", "25$\degree$-35$\degree$S",
           "5$\degree$S-5$\degree$N", "25$\degree$-35$\degree$N", "55$\degree$-65$\degree$N", ">85$\degree$N"]
 lat_inds = [slice(0, 3), slice(9,12), slice(19,23), slice(30,34),
     slice(41,45), slice(52,55), slice(62, 64)]
-print(np.linspace(0, len(lat_inds)))
-print(len(np.linspace(0, len(lat_inds))))
 cmap = cm.coolwarm(np.linspace(0, len(lat_inds)))
 
 fig, axs = plt.subplots(1, 2 , figsize=(10, 5), sharey = True)
-# Zonal
+## Zonal
 ax = axs[0]
 plt.sca(ax)
 for i, lat_ind in enumerate(lat_inds):
+    # Select all gwfu in this latitude range
     gwfu_ad99_lat = gwfu_ad99[..., lat_ind]
+    # Reshape to (N, npfull)
     nlat_ind = gwfu_ad99_lat.shape[-1]
     gwfu_ad99_lat = np.moveaxis(gwfu_ad99[..., lat_ind], -1, 0).reshape((ntime*nlon*nlat_ind, npfull))
     gwfu_preds_lat = np.moveaxis(gwfu_preds[..., lat_ind], -1, 1).reshape((n_seeds, ntime*nlon*nlat_ind, npfull))
+    # CRPS
     crps_u = crps(gwfu_ad99_lat, gwfu_preds_lat)
-
+    # Plot
     plt.semilogy(crps_u, pfull,  color=cmap[i], label=labels[i])
+# Labels, axis, legends
 plt.xlabel("Zonal GWD (ms$^{-2}$)")
 plt.title("Zonal GWD")
 ax.invert_yaxis()
 plt.ylabel("Pressure (hPa)")
 plt.legend()
 plt.axis(xmin=0, xmax=5e-6, ymin=3e2, ymax=1)
+# Add a) label
+plt.text(x=-0.15, y=1.01, s="a)", fontsize=16, transform=ax.transAxes)
 
-# Meridional
+## Meridional
 ax = axs[1]
 plt.sca(ax)
 for i, lat_ind in enumerate(lat_inds):
+    # Select all gwfv in this latitude range
     gwfv_ad99_lat = gwfv_ad99[..., lat_ind]
+    # Reshape to (N, npfull)
     nlat_ind = gwfv_ad99_lat.shape[-1]
     gwfv_ad99_lat = np.moveaxis(gwfv_ad99[..., lat_ind], -1, 0).reshape((ntime*nlon*nlat_ind, npfull))
     gwfv_preds_lat = np.moveaxis(gwfv_preds[..., lat_ind], -1, 1).reshape((n_seeds, ntime*nlon*nlat_ind, npfull))
+    # CRPS
     crps_v = crps(gwfv_ad99_lat, gwfv_preds_lat)
+    # Plot
     plt.semilogy(crps_v, pfull,  color=cmap[i], label=labels[i])
+# Labels, axis, legends
 plt.xlabel("Meridional GWD (ms$^{-2}$)")
 plt.title("Meridional GWD")
 plt.legend()
 plt.axis(xmin=0, xmax=5e-6) 
+# Add b) label
+plt.text(x=-0.1, y=1.01, s="b)", fontsize=16, transform=ax.transAxes)
+
 plt.suptitle("Continuous Ranked Probability Score")
 save_plotname = f"{save_dir}/GWD_err_CRPS_profile.png"
 plt.savefig(save_plotname)
