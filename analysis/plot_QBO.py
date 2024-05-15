12b356e7ab16ab454c88c8ff13df690b3d22fd11
lm2612 <lauraman@stanford.edu>
2024-05-15 14:57:10 -0700

    colormap changed to BrBG

--- analysis/plot_QBO.py
+++ analysis/plot_QBO.py
@@ -7,7 +7,7 @@ def plot_QBO(time, pfull, u_QBO, ax=None, cbar=True, x_axis=True, y_axis=True):
     if ax == None:
         fig, ax = plt.subplots(nrows=1,ncols=1, figsize=(8, 3))
     plt.sca(ax)
-    plt.contourf(time, pfull, u_QBO.T, levels=np.arange(-45., 45.1, 5), cmap="RdBu_r", extend="both")
+    plt.contourf(time, pfull, u_QBO.T, levels=np.arange(-45., 45.1, 5), cmap="BrBG_r", extend="both")
     if x_axis:
         plt.xlabel("Time (years)")
     if y_axis:
